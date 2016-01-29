26ce02269f992b3c8b1977a8b30a4e4a106fd3f2
Matt Spaulding <mrmattspaulding@gmail.com>
2016-01-28 21:55:21 -0500

    Cleaned up res.

--- ButtNagger/platforms/android/assets/www/cordova_plugins.js
+++ ButtNagger/platforms/android/assets/www/cordova_plugins.js
@@ -13,12 +13,20 @@ module.exports = [
         "merges": [
             "navigator.notification"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.splashscreen/www/splashscreen.js",
+        "id": "org.apache.cordova.splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "org.apache.cordova.dialogs": "0.3.0"
+    "org.apache.cordova.dialogs": "0.3.0",
+    "org.apache.cordova.splashscreen": "1.0.0"
 }
 // BOTTOM OF METADATA
 });
---
+++ ButtNagger/platforms/windows/www/js/services.js
@@ -0,0 +1,40 @@
+﻿angular.module('starter.services', [])
+
+.factory('Nags', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var nags = [{
+    id: 0,
+    name: '10 Squats',
+    lastText: 'You can do it!',
+    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+  }, {
+    id: 1,
+    name: '5 Lunges',
+    lastText: 'Work it',
+    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+  }, {
+    id: 2,
+    name: '20 Wide squats',
+    lastText: 'Feel the burn?',
+    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+  }];
+
+  return {
+    all: function() {
+      return nags;
+    },
+    remove: function(nag) {
+      nags.splice(nags.indexOf(nag), 1);
+    },
+    get: function(nagId) {
+      for (var i = 0; i < nags.length; i++) {
+        if (nags[i].id === parseInt(nagId)) {
+          return nags[i];
+        }
+      }
+      return null;
+    }
+  };
+});

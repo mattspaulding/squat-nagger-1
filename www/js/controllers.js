0379062512ece383ffb56ade52e06682c2c4dd84
Matt Spaulding <mrmattspaulding@gmail.com>
2016-01-29 14:31:08 -0500

    Testing scheduled nags.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -23,34 +23,35 @@ angular.module('starter.controllers', [])
         enableFriends: true
     };
 
-     $scope.scheduleNagger = function (nagger) {
-         debugger;
-         $scope.nags = Nags.all();
-         var nag=$scope.nags[0];
-         var nag1=$scope.nags[1];
-       // var sound = device.platform == 'Android' ? 'file://sound.mp3' : 'file://beep.caf';
-         var date1 = new Date();
-         var oneMinute=new Date();
-         oneMinute.setMinutes(oneMinute.getMinutes()+1);
-         cordova.plugins.notification.local.schedule([{
-             id: nag.id,
-             title: nag.name,
-             message: nag.lastText,
-             at: date1,
-             sound: 'file://beep.caf',
-             icon: "https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png",
-             data: { nagId: nag.id }
-         },{
-             id: nag1.id,
-             title: nag1.name,
-             message: nag1.lastText,
-             at: oneMinute,
-             sound: 'file://beep.caf',
-             icon: "https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png",
-             data: { nagId: nag1.id }
-         }]);
+    $scope.scheduleNagger = function (nagger) {
+        debugger;
+        $scope.nags = Nags.all();
+        var nag = $scope.nags[0];
+        var nag1 = $scope.nags[1];
+        // var sound = device.platform == 'Android' ? 'file://sound.mp3' : 'file://beep.caf';
+        var date1 = new Date();
+        var oneMinute = new Date();
+        oneMinute.setMinutes(oneMinute.getMinutes() + 1);
+        cordova.plugins.notification.local.schedule([{
+            id: nag.id,
+            title: nag.name,
+            message: nag.lastText,
+            at: date1,
+            sound: 'file://beep.caf',
+            icon: "https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png",
+            data: { nagId: nag.id }
+        }, {
+            id: nag1.id,
+            title: nag1.name,
+            message: nag1.lastText,
+            at: oneMinute,
+            sound: 'file://beep.caf',
+            icon: "https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png",
+            data: { nagId: nag1.id }
+        }]);
 
         cordova.plugins.notification.local.on("click", function (notification) {
             $state.go('tab.nag-detail', { nagId: notification.id });
         });
+    };
 });

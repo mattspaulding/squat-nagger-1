bd98f5402b537d4b942877ec3b433d52c70a101f
Matt Spaulding <mrmattspaulding@gmail.com>
2016-01-30 02:32:23 -0500

    Debugging.

--- www/js/services.js
+++ www/js/services.js
@@ -4,7 +4,7 @@ angular.module('starter.services', [])
     // Might use a resource here that returns a JSON array
 
     var naggers = [{
-        id:1,
+        id: 1,
         name: 'Marin',
         face: 'https://cdnil0.fiverrcdn.com/photos/2170980/original/DSC_6769.jpg?1448408453',
         level: 'Easy',
@@ -14,21 +14,21 @@ angular.module('starter.services', [])
             message: 'Let\'s start off easy.',
             date: 0,
             hour: 2,
-            minute:27
+            minute: 27
         }, {
             id: 101,
             title: '15 Squats',
             message: 'Let\'s do more.',
             date: 0,
             hour: 2,
-            minute: 28
+            minute: 34
         }, {
             id: 102,
             title: '20 Squats',
             message: 'Let\'s burn it.',
             date: 0,
             hour: 2,
-            minute:29
+            minute: 35
         }]
     }];
 
@@ -48,35 +48,34 @@ angular.module('starter.services', [])
             var currentNagger = JSON.parse(window.localStorage.getItem("currentNagger"));
             for (var i = 0; i < currentNagger.nags.length; i++) {
                 if (currentNagger.nags[i].id === nag.id) {
-                    currentNagger.nags.splice(i,1);
+                    currentNagger.nags.splice(i, 1);
                 }
             }
 
-          // currentNagger.nags.splice(currentNagger.nags.indexOf(nag), 1);
-           window.localStorage.setItem("currentNagger", JSON.stringify(currentNagger));
+            // currentNagger.nags.splice(currentNagger.nags.indexOf(nag), 1);
+            window.localStorage.setItem("currentNagger", JSON.stringify(currentNagger));
         },
         setCurrentNaggerByName: function (name) {
             for (var i = 0; i < naggers.length; i++) {
                 if (naggers[i].name === name) {
-                    window.localStorage.setItem("currentNagger",JSON.stringify(naggers[i]));
+                    window.localStorage.setItem("currentNagger", JSON.stringify(naggers[i]));
                     return naggers[i];
                 }
             }
             throw "Couldn't find object with name: " + name;
         },
-        getCurrentNagger:function(){
+        getCurrentNagger: function () {
             return JSON.parse(window.localStorage.getItem("currentNagger"));
         },
         enable: function (nagId) {
             var currentNagger = JSON.parse(window.localStorage.getItem("currentNagger"));
             for (var i = 0; i < currentNagger.nags.length; i++) {
                 if (currentNagger.nags[i].id === parseInt(nagId)) {
-                     currentNagger.nags[i].enabled=true;
+                    currentNagger.nags[i].enabled = true;
+                    window.localStorage.setItem("currentNagger", JSON.stringify(currentNagger));
                 }
             }
-            return null;
-        }
-  ,
+        },
         get: function (nagId) {
             var currentNagger = JSON.parse(window.localStorage.getItem("currentNagger"));
             for (var i = 0; i < currentNagger.nags.length; i++) {

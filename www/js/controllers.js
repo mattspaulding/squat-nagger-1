ee7d0906cd0b8bf01e1273abcd9eabc9d436fcf4
Matt Spaulding <mrmattspaulding@gmail.com>
2016-01-30 02:05:10 -0500

    Add trigger.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -43,58 +43,23 @@ angular.module('starter.controllers', [])
     $scope.scheduleNagger = function (nagger) {
 
         cordova.plugins.notification.local.clearAll(function () {
-            //alert("done");
+            $scope.nagger = Nags.setCurrentNaggerByName('Marin');
+            var notifications = [];
+            $scope.nagger.nags.forEach(function (nag, index) {
+                var date = new Date();
+                date.setDate(date.getDate() + nag.date);
+                date.setHours(nag.hour);
+                date.setMinutes(nag.minute);
+                var notification = {};
+                notification.id = nag.id;
+                notification.title = nag.title;
+                notification.message = nag.message;
+                notification.date = date;
+                notifications.push(notification);
+            });
+            cordova.plugins.notification.local.schedule(notifications);
         }, this);
-        $scope.nagger = Nags.setCurrentNaggerByName('Marin');
-        var notifications = [];
-        $scope.nagger.nags.forEach(function (nag, index) {
-            var date = new Date();
-            date.setDate(date.getDate() + nag.date);
-            date.setHours(nag.hour);
-            date.setMinutes(nag.minute);
-            var notification = {};
-            notification.id = nag.id;
-            notification.title = nag.title;
-            notification.message = nag.message;
-            notification.date = date;
-            notifications.push(notification);
-        });
-        cordova.plugins.notification.local.schedule(notifications);
-        //$scope.nags = Nags.all();
-        //var nag0 = $scope.nags[0];
-        //var nag1 = $scope.nags[1];
-        //var nag2 = $scope.nags[2];
-        //var nag3 = $scope.nags[3];
-        //// var sound = device.platform == 'Android' ? 'file://sound.mp3' : 'file://beep.caf';
-        //var date = new Date();
-        //var date1 = new Date();
-        //date1.setMinutes(date.getMinutes() + 1);
-        //var date2 = new Date();
-        //date2.setMinutes(date.getMinutes() + 2);
-        //var date3 = new Date();
-        //date3.setMinutes(date.getMinutes() + 3);
-        //cordova.plugins.notification.local.schedule([{
-        //    id: nag1.id,
-        //    title: 'notif1',// nag1.name,
-        //    message: nag1.lastText,
-        //    at: date1
-        //}, {
-        //    id: nag2.id,
-        //    title: 'notif2',// nag1.name,
-        //    message: nag2.lastText,
-        //    at: date2,
-        //    sound: 'file://beep.caf',
-        //    icon: "https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png",
-        //    data: { nagId: nag2.id }
-        //}, {
-        //    id: nag3.id,
-        //    title: 'notif3',// nag1.name,
-        //    message: nag3.lastText,
-        //    at: date3,
-        //    sound: 'file://beep.caf',
-        //    icon: "https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png",
-        //    data: { nagId: nag3.id }
-        //}]);
+
 
         $ionicHistory.nextViewOptions({
             disableBack: true
@@ -105,5 +70,9 @@ angular.module('starter.controllers', [])
             $state.go('tab.nag-detail', { nagId: notification.id });
         });
 
+
+        cordova.plugins.notification.local.on("trigger", function (notification) {
+            alert("triggered: " + notification.id);
+        });
     };
 });

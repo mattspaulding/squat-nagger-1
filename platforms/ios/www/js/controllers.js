0a95fb08e4683deff4a4f73b53b26fbe4aba99df
Matt Spaulding <mrmattspaulding@gmail.com>
2016-02-05 11:38:21 -0500

    Dont add first notifications.

--- platforms/ios/www/js/controllers.js
+++ platforms/ios/www/js/controllers.js
@@ -67,93 +67,99 @@ angular.module('starter.controllers', ["ionic", "ngCordova"])
 
 })
 
-.controller('NaggersCtrl', function ($scope,$rootScope, $ionicPlatform, Nags, $ionicHistory, $state, $ionicPopup, $cordovaLocalNotification) {
-    $scope.settings = {
-        enableFriends: true
-    };
+.controller('NaggersCtrl',['$scope', '$rootScope', '$ionicPlatform', '$cordovaLocalNotification', '$ionicHistory', '$state', '$ionicPopup','Nags', function ($scope,$rootScope, $ionicPlatform, $cordovaLocalNotification, $ionicHistory, $state, $ionicPopup,Nags ) {
+    $ionicPlatform.ready(function () {
+        alert('ionic ready');
+        $scope.settings = {
+            enableFriends: true
+        };
 
-    $scope.naggers = Nags.getAllNaggers();
-    $scope.nagger = Nags.getCurrentNagger();
+        $scope.naggers = Nags.getAllNaggers();
+        $scope.nagger = Nags.getCurrentNagger();
 
-    $scope.cancelNagger = function () {
-        Nags.clearBadgeNumber();
-        Nags.cancelCurrentNagger();
-        $scope.nagger = null;
-    };
+        $scope.cancelNagger = function () {
+            Nags.clearBadgeNumber();
+            Nags.cancelCurrentNagger();
+            $scope.nagger = null;
+        };
 
-    // A confirm dialog
-    $scope.showConfirm = function () {
-        var confirmPopup = $ionicPopup.confirm({
-            title: 'Cancel Nagger',
-            template: 'Are you sure you want to cancel this Nagger?',
-            cancelText: 'No',
-            okText: 'Yes'
-        });
-
-        confirmPopup.then(function (res) {
-            if (res) {
-                $scope.cancelNagger();
-            } else {
-                //do nothing
-            }
-        });
-    };
-
-    $scope.scheduleNagger = function (naggerName) {
-        //window.plugin.notification.local.cancelAll(function () {
-        $cordovaLocalNotification.cancelAll().then(function (result) {
-            $scope.nagger = Nags.setCurrentNaggerByName(naggerName);
-            var notifications = [];
-            $scope.nagger.nags.forEach(function (nag, index) {
-                var date = new Date();
-                date.setDate(date.getDate() + nag.day);
-                date.setHours(nag.hour);
-                date.setMinutes(nag.minute);
-                date.setSeconds(0);
-                $scope.nagger.nags[index].date = date;
-                var guid = index;//Math.floor((Math.random() * 999999999999999) + 1);
-                $scope.nagger.nags[index].id = guid;
-                var notification = {};
-                notification.id = guid;
-                notification.title = nag.title;
-                notification.text = nag.message;
-                notification.date = date;
-                notification.sound = 'file://sounds/cork-pop.wav',
-
-                 notifications.push(notification);
-          
+        // A confirm dialog
+        $scope.showConfirm = function () {
+            var confirmPopup = $ionicPopup.confirm({
+                title: 'Cancel Nagger',
+                template: 'Are you sure you want to cancel this Nagger?',
+                cancelText: 'No',
+                okText: 'Yes'
             });
-            $scope.nagger = Nags.setCurrentNaggerByName(naggerName);
-            $cordovaLocalNotification.schedule(notifications);
-            //cordova.plugins.notification.local.schedule(notifications);
-            $cordovaLocalNotification.clearAll();
 
-            $state.go('tab.nag-detail', { nagId: $scope.nagger.nags[0].id });
+            confirmPopup.then(function (res) {
+                if (res) {
+                    $scope.cancelNagger();
+                } else {
+                    //do nothing
+                }
+            });
+        };
 
-        }, this);
+        $scope.scheduleNagger = function (naggerName) {
+            //window.plugin.notification.local.cancelAll(function () {
+            $cordovaLocalNotification.cancelAll().then(function (result) {
+                $scope.nagger = Nags.setCurrentNaggerByName(naggerName);
+                var notifications = [];
+                $scope.nagger.nags.forEach(function (nag, index) {
+                    var date = new Date();
+                    date.setDate(date.getDate() + nag.day);
+                    date.setHours(nag.hour);
+                    date.setMinutes(nag.minute);
+                    date.setSeconds(0);
+                    $scope.nagger.nags[index].date = date;
+                    var guid = index;//Math.floor((Math.random() * 999999999999999) + 1);
+                    $scope.nagger.nags[index].id = guid;
+                    var notification = {};
+                    notification.id = guid;
+                    notification.title = nag.title;
+                    notification.text = nag.message;
+                    notification.date = date;
+                    notification.sound = 'file://sounds/cork-pop.wav';
+                    if (notification.date > new Date())
+                    {
+                        notifications.push(notification);
+                    }
+          
+                });
 
-        $rootScope.$on('$cordovaLocalNotification:click', function (event, notification, state) {
-            $state.go('tab.nag-detail', { nagId: notification.id });
-        });
-        //cordova.plugins.notification.local.on("click", function (notification) {
-        //    $state.go('tab.nag-detail', { nagId: notification.id });
-        //});
+                $scope.nagger = Nags.setCurrentNaggerByName(naggerName);
+                $cordovaLocalNotification.schedule(notifications).then(function (result) {;
+                    $cordovaLocalNotification.clearAll();
+                    alert('cleared');
+                });
+                $state.go('tab.nag-detail', { nagId: $scope.nagger.nags[0].id });
 
-        $rootScope.$on('$cordovaLocalNotification:trigger', function (event, notification, state) {
-            Nags.setBadgeNumber();
-        });
-        //cordova.plugins.notification.local.on("trigger", function (notification) {
-        //    Nags.setBadgeNumber();
-        //});
+            }, this);
 
-    //    $rootScope.$on('$cordovaLocalNotification:schedule',function (event, notification, state) {
-    //        $cordovaLocalNotification.clearAll();
-    //});
+            $rootScope.$on('$cordovaLocalNotification:click', function (event, notification, state) {
+                $state.go('tab.nag-detail', { nagId: notification.id });
+            });
+            //cordova.plugins.notification.local.on("click", function (notification) {
+            //    $state.go('tab.nag-detail', { nagId: notification.id });
+            //});
 
-        $ionicHistory.nextViewOptions({
-            disableBack: true
-        });
+            $rootScope.$on('$cordovaLocalNotification:trigger', function (event, notification, state) {
+                Nags.setBadgeNumber();
+            });
+            //cordova.plugins.notification.local.on("trigger", function (notification) {
+            //    Nags.setBadgeNumber();
+            //});
+
+            //    $rootScope.$on('$cordovaLocalNotification:schedule',function (event, notification, state) {
+            //        $cordovaLocalNotification.clearAll();
+            //});
+
+            $ionicHistory.nextViewOptions({
+                disableBack: true
+            });
 
 
-    };
-});
+        };
+    });
+}]);
